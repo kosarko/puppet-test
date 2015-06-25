5a050dd1b4e97037a2e6c480175c10ae9e2c8a79
Ondrej Kosarko <ko_ok@centrum.cz>
2015-06-25 11:48:34 +0000

    sources/installations dirs

--- manifests/first.pp
+++ manifests/first.pp
@@ -6,3 +6,20 @@ package { 'git':
     require => Exec['apt-update'],
     ensure => latest,
 }
+
+$my_dirs = [ "/opt/installations", "/opt/sources" ]
+file { $my_dirs:
+    ensure => directory,
+}
+
+file { '/installations':
+    ensure => link,
+    target => '/opt/installations',
+    require => File['/opt/installations'],
+}
+
+file { '/sources':
+    ensure => link,
+    target => '/opt/sources',
+    require => File['/opt/sources'],
+}
